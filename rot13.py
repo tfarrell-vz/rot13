3cf0e23faa5d5557594a913407bb5462e1d65e65
tfarrell-vz <farrell@abiresearch.com>
2014-11-12 18:08:54 -0500

    Basic form for textarea.

--- rot13.py
+++ rot13.py
@@ -6,21 +6,24 @@ import webapp2
 # Construct the template directory path and assign it to `template_dir`
 # __file__ refers to this file
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+							   autoescape = True)
 
 # Make a Handler class from which other Handlers can inherit. 
 class Handler(webapp2.RequestHandler):
-	def write(self, content):
-		self.response.out.write(content)
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
 
-	def render_as_string(self):
-		return None
+	def render_as_string(self, template, **kw):
+		t = jinja_env.get_template(template)
+		return t.render(kw)
 
-	def render(self):
-		return None
+	def render(self, template, **kw):
+		self.write(self.render_as_string(template, **kw))
 
 class MainPage(Handler):
 	def get(self):
-		self.write("Hello, world!")
+		self.render('index.html')
 
 app = webapp2.WSGIApplication([
 							    ('/', MainPage),
