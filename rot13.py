cc1c7caaa3519b30014d58b142fd6e6a2b463dc6
tfarrell-vz <farrell@abiresearch.com>
2014-11-17 09:44:45 -0500

    functional in place rot13 encoding

--- rot13.py
+++ rot13.py
@@ -10,46 +10,45 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 							   autoescape = True)
 
-# Make a Handler class from which other Handlers can inherit. 
+# Encoding could have been handled in the standard library. 
+def rot13_encode(text):
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    rot13_text = ""
+    for char in text:
+        if char.isalpha():
+            if char.islower():
+                index = lowercase.find(char)
+                index = (index+13) % 26
+                rot13_text += lowercase[index]
+            else: 
+                index = uppercase.find(char)
+                index = (index+13) % 26
+                rot13_text += uppercase[index]
+        else:
+            rot13_text += char
+    return rot13_text
+
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
 		self.response.out.write(*a, **kw)
 
 	def render_as_string(self, template, **kw):
 		t = jinja_env.get_template(template)
-		return t.render(kw)
+		return t.render(**kw)
 
 	def render(self, template, **kw):
 		self.write(self.render_as_string(template, **kw))
 
-class MainPage(Handler):
-	def get(self):
-		self.render('index.html')
+class ROT13Handler(Handler):
+    def get(self):
+        self.render('index.html')
 
-	def post(self):
-		text = self.request.get('text')
-
-		lowercase = string.ascii_lowercase
-		uppercase = string.ascii_uppercase
-
-		rot13_text = ""
-		for char in text:
-			if char.isalpha():
-				if char.islower():
-					index = lowercase.find(char)
-					index = (index+13) % 26
-					rot13_text += lowercase[index]
-				else: 
-					index = uppercase.find(char)
-					index = (index+13) % 26
-					rot13_text += uppercase[index]
-			else:
-				rot13_text += char
-
-
-			
-		self.render('index.html', input_text=text, rot13_text=rot13_text)
+    def post(self):
+        text = self.request.get('text')
+        rot13_text = rot13_encode(text)
+        self.render('index.html', input_text=text, rot13_text=rot13_text)
 
 app = webapp2.WSGIApplication([
-							    ('/', MainPage),
+							    ('/rot13', ROT13Handler),
 								], debug=True)
